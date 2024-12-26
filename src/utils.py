76e0a5acadc9b4288d63ff1858aa2160687ab745
Veeru <singuveeru101@gmail.com>
2024-12-26 13:47:20 -0500

    model trained

--- src/utils.py
+++ src/utils.py
@@ -21,3 +21,33 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
+def evaluate_models(x_train, y_train,x_test,y_test,models,params):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(x_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(x_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(x_train)
+
+            y_test_pred = model.predict(x_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
