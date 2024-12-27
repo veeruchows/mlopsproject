de0ce8491ddab7af4bff50e3802a913a7e0e4f56
Veeru <singuveeru101@gmail.com>
2024-12-27 17:52:26 -0500

    deployment done

--- src/utils.py
+++ src/utils.py
@@ -21,25 +21,26 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
-def evaluate_models(x_train, y_train,x_test,y_test,models,params):
+    
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            para=params[list(models.keys())[i]]
+            para=param[list(models.keys())[i]]
 
             gs = GridSearchCV(model,para,cv=3)
-            gs.fit(x_train,y_train)
+            gs.fit(X_train,y_train)
 
             model.set_params(**gs.best_params_)
-            model.fit(x_train,y_train)
+            model.fit(X_train,y_train)
 
             #model.fit(X_train, y_train)  # Train model
 
-            y_train_pred = model.predict(x_train)
+            y_train_pred = model.predict(X_train)
 
-            y_test_pred = model.predict(x_test)
+            y_test_pred = model.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
 
@@ -48,6 +49,14 @@ def evaluate_models(x_train, y_train,x_test,y_test,models,params):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
